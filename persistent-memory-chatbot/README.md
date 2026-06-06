7fb712c30a9b53ae9505eec461e4e8dd5c175a54
andrewino <andrewino@proton.me>
2026-06-07 01:43:48 +0200

    Add files via upload

---
+++ persistent-memory-chatbot/README.md
@@ -0,0 +1,2 @@
+# persistent-memory-chatbot
+persistent memory chatbot system. It is made up of two AIs: one that chats with the user and one that analyzes conversations to build a user profile, allowing for increasingly personalized responses over time.
---
+++ persistent-memory-chatbot/secondaAI.py
@@ -0,0 +1,37 @@
+
+import ollama
+import json
+
+history = []
+
+def save_history():
+    with open("profilo.json", "w") as f:
+        json.dump(reply, f)
+
+def load_history():
+    global history
+    try:
+        with open("chat_history.json", "r") as f:
+            history = json.load(f)
+    except FileNotFoundError:
+        history = []
+
+load_history()
+
+def main():
+    global reply
+    stringa = open("prompt.txt", encoding="utf-8").read()
+    
+    # Combina il prompt con la historyå
+    messaggio = stringa + "\n\n" + json.dumps(history, ensure_ascii=False)
+    
+    response = ollama.chat(
+        model="gemma3:4b",
+        messages=[{"role": "user", "content": messaggio}]
+    )
+    
+    reply = response.message.content
+    
+    save_history()
+
+main()
